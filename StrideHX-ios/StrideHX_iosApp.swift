1163a824be07b5bedc40c812bcb39b47fcd692c7
Edmond <mredmondwang@gmail.com>
2023-02-07 15:28:49 -0800

    Initial Commit

---
+++ StrideHX-ios/StrideHX_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideHX_iosApp.swift
+//  StrideHX-ios
+//
+//  Created by OAA on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct StrideHX_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
